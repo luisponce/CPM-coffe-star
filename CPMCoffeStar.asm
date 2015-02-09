845a8232379383894f92a4a71de9dcf52dd48286
Mateo Olaya Bernal <dev.mateo.olaya@me.com>
2015-02-09 17:40:58 -0500

    Se calcula el subsidio de transporte en base del 5% del sueldo basico

--- CPMCoffeStar.asm
+++ CPMCoffeStar.asm
@@ -19,9 +19,9 @@
  ; EEC = Reservado
  ; EED = Numero salarios minimos (16 bits)
  ; EEE = Flag de salario minimo no entero (0 si es entero, 1 si es por ejm 2.4)
- ; EEF =
- ; EEG = Reservado
- ;
+ ; EEF = Porsentaje para calcular el subsidio de transporte (32 bits)
+ ; EF0 = Reservado
+ ; EF1 = Temporal
 
  ; Home de la aplicacion
 
@@ -33,16 +33,16 @@
  MSG   3. Salir
  LDT INGRESE LA OPCION:
 
- CMP EE1 	; Compara AX con 1
- JEQ 010	; Si son iguales salta a ingresar nomina
- CMP EE2 	; Compara AX con 2
- JEQ 02B	; Si son iguales salta a totales
- CMP EE3 	; Compara AX con 3
- JEQ 00F 	; Si son iguales salta a terminar programa
+ CMP EE1        ; Compara AX con 1
+ JEQ 010        ; Si son iguales salta a ingresar nomina
+ CMP EE2        ; Compara AX con 2
+ JEQ 02B        ; Si son iguales salta a totales
+ CMP EE3        ; Compara AX con 3
+ JEQ 00F        ; Si son iguales salta a terminar programa
  MSG Ingrese una opcion valida
- JMP 006 	; Salta a ingresar opcion
+ JMP 006        ; Salta a ingresar opcion
 
- HLT 		; Terminar programa
+ HLT            ; Terminar programa
 
  ; Futura implementacion
 
@@ -56,44 +56,52 @@
  STF EE5
 
  ; OPERACIONES
- LDF EE5     	; Pasar de Sueldo basico empleados a Ax-Bx 32bits
- DIVF EEB    	; Ax-Bx dividido SMLMV (32bits) y almacena en Ax en 32bits
- FTOI       	; Pasa de flotante de 32bits a Entero de 16bits
+ LDF EE5        ; Pasar de Sueldo basico empleados a Ax-Bx 32bits
+ 
+ STF EE9        ; Guardar sueldo basico en sueldo
+ DIVF EEB       ; Ax-Bx dividido SMLMV (32bits) y almacena en Ax en 32bits
+ MOV EF1,CX     ; Pasa el decimal a un temporal
+ FTOI           ; Pasa de flotante de 32bits a Entero de 16bits
  MOV EED,AX     ; Guarda lo que esta en AX en Numero salarios minimos en 16bits
- MOV AX, CX
- CMP EE0        ; comparar si el reciduo de la div es cero
- JEQ 021         ; SI es cero salte a setFlag
- MOV EEE, EE1    ; SINO carga 1 en El flag de salario minimo no entero
- JMP 022         ; salta a fin si
- MOV EEE, EE0    ; ENTONCES SetFlag, pone el flag de salario minimo no entero en cero
- NOP ; FIN SI
+ MOV AX,EF1
+ CMP EE0         ; comparar si el reciduo de la div es cero
+ JEQ 024         ; SI es cero salte a ENTONCES
+ MOV AX,EE1      ; SINO carga 1 en AX
+ MOV EEE,AX     ; Pone el flag en 1 (tiene decimales)
+ JMP 026         ; salta a fin si
+ 
+ MOV AX,EE0     ; ENTONCES SetFlag, pone AX en cero
+ MOV EEE,AX     ; carga 0 en el flag (no tiene decimales)
+ NOP             ; FIN SI
  
  ; subsidio de transporte
- MOV AX, EE2     ; Guarda 2 en AX
+ MOV AX,EE2     ; Guarda 2 en AX
  CMP EED         ; Compara el numero de salarios minimos con 2
- JME             ; SI el numero de salarios minimos es menor que 2 ir a calcular subsidio
- JEQ             ; SINO SI es igual a dos ir a comparar flag
- JMP ; FIN SI
+ JMA 030         ; SI el numero de salarios minimos es menor que 2 ir a calcular subsidio
+ JEQ 02C         ; SINO SI es igual a dos ir a comparar flag
+ JMP 034         ; FIN SI
  
  ; compar flag
- CLA             ; AX=0
- CMP EEE         ; compara el flag con 0 para saber si tiene decimales
- JEQ             ; SI tiene decimales ir a calcular subsidio
- JMP ; Fin si
+ MOV AX,EE1      ; AX=0
+ CMP EEE         ; compara el flag con 1 para saber si tiene decimales
+ JEQ 030         ; SI tiene decimales ir a calcular subsidio
+ JMP 034         ; Fin si
  
  ; calcular subsidio
- LDF EE4         ; Guarda el sueld basico en AX
- MULF EEF
- 
- 
- 
- 
+ LDF EE5         ; Guarda el sueldo basico en AX
+ MULF EEF        ; Multiplica el sueldo basico por el porcentaje (EEF) y almacena en AX
+ ADDF EE9        ; Suma al sueldo actual el subsidio de transporte
+ STF EE9      ; Actualiza el sueldo actual en el espacio de memoria correspondiente
+
+ LDF EE9
+ OUT 1,AX
+ MSG subsidio calculado
  
  
 
  MSG Valor a pagar:
- LDF EE7 	; Cargar total pago a empleado del grupo
- OUT 1,AX 	; Escribir total pago a empleado del grupo
+ LDF EE7        ; Cargar total pago a empleado del grupo
+ OUT 1,AX       ; Escribir total pago a empleado del grupo
  NOP
  
  MSG ------------------------------------------
@@ -103,11 +111,11 @@
  LDT
 
  CMP EE1
- JEQ 010 	; Si es 1 salta a Ingresar nomina
+ JEQ 010        ; Si es 1 salta a Ingresar nomina
  CMP EE2
- JEQ 000 	; Si es 2 salta a home
+ JEQ 000        ; Si es 2 salta a home
  MSG Ingrese una opcion valida
- JMP 01F 	; Si no ingresa una opcion valida
+ JMP 03B        ; Si no ingresa una opcion valida
 
  MSG Totales
  MOV AX,EED
